ef2b0277a66ca413bc9c8b14fa5cc8c7b8861daa
RentalCat <RentalCat@users.noreply.github.com>
2019-11-15 19:20:19 +0900

    add black

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 check:
 	flake8 .
 	mypy --strict .
+	black --check .
 	python -m pytest --cov=flake8_reassignment_checker --cov-report=xml
--- tests/conftest.py
+++ tests/conftest.py
@@ -5,15 +5,11 @@ from typing import List, Tuple
 from flake8_reassignment_checker.checker import ReassignmentChecker
 
 
-def run_validator_for_test_file(
-    filename: str,
-) -> List[Tuple[int, int, str, type]]:
+def run_validator_for_test_file(filename: str,) -> List[Tuple[int, int, str, type]]:
     test_file_path = os.path.join(
-        os.path.dirname(os.path.abspath(__file__)),
-        'test_files',
-        filename,
+        os.path.dirname(os.path.abspath(__file__)), "test_files", filename,
     )
-    with open(test_file_path, 'r') as file_handler:
+    with open(test_file_path, "r") as file_handler:
         raw_content = file_handler.read()
     tree = ast.parse(raw_content)
     checker = ReassignmentChecker(tree=tree, filename=filename)
