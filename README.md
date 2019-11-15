d84fd9ca0189efea812c8ba9cb0e9ef725a500ef
RentalCat <RentalCat@users.noreply.github.com>
2019-11-15 17:15:56 +0900

    add ci

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # flake8-reassignment-checker
+
+[![Build Status](https://travis-ci.com/RentalCat/flake8-reassignment-checker.svg?token=Vmz2Dcr13iE5dECVhdbN&branch=master)](https://travis-ci.com/RentalCat/flake8-reassignment-checker)
+
 Flake8 plugin that checks reassignment values.
---
+++ flake8_reassignment_checker/checker.py
@@ -0,0 +1,14 @@
+import ast
+from typing import Iterator, Tuple
+from . import __version__ as version
+
+
+class ReassignmentChecker:
+    name = "reassignment-checker"
+    version = version
+
+    def __init__(self, tree: ast.Module, filename: str) -> None:
+        self.tree = tree
+
+    def run(self) -> Iterator[Tuple[int, int, str, type]]:
+        pass
