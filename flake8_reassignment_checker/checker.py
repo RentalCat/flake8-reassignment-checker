a834caad735b9f42a02533e93b9813d931d21ead
RentalCat <RentalCat@users.noreply.github.com>
2019-11-20 13:16:17 +0900

    pass the test: cc94c44

--- flake8_reassignment_checker/checker.py
+++ flake8_reassignment_checker/checker.py
@@ -1,5 +1,5 @@
 import ast
-from typing import Iterator, Tuple, NamedTuple, Set, Optional
+from typing import Iterator, Tuple, NamedTuple, Optional, List
 from . import __version__ as version
 
 
@@ -34,29 +34,33 @@ class ReassignmentChecker:
 
         if isinstance(child, ast.Assign):
             for target in child.targets:
-                yield _Valiable(cls._get_assign_name(target), child.lineno, "variable")
+                for name in cls._get_assign_names(target):
+                    yield _Valiable(name, child.lineno, "variable")
         elif isinstance(child, ast.AnnAssign):
-            yield _Valiable(cls._get_assign_name(child.target), child.lineno, "variable")
+            for name in cls._get_assign_names(child.target):
+                yield _Valiable(name, child.lineno, "variable")
 
         yield from cls._analysis_valiables(childs)
 
     @classmethod
-    def _get_assign_name(cls, target: ast.AST) -> str:
+    def _get_assign_names(cls, target: ast.AST) -> List[str]:
         if isinstance(target, ast.Name):
-            return target.id
-        elif isinstance(target, ast.Attribute):
-            return f"{cls._get_assign_name(target.value)}.{target.attr}"
-        raise Exception()
+            return [target.id]
+        # elif isinstance(target, ast.Attribute):
+        #     return [f"{cls._get_assign_names(target.value)}.{target.attr}"]
+        elif isinstance(target, ast.Tuple):
+            return [n for e in target.elts for n in cls._get_assign_names(e)]
+        raise Exception(f"unknown ast type: {type(target)}, [{target._fields}]")
 
     @classmethod
     def _check_names(
-        cls, targets: Iterator[_Valiable], valiables: Optional[Set[_Valiable]] = None
+        cls, targets: Iterator[_Valiable], valiables: Optional[List[_Valiable]] = None
     ) -> Iterator[_LintOutput]:
         target = next(targets, None)
         if not target:
             return
 
-        _valiables: Set[_Valiable] = valiables or set()
+        _valiables: List[_Valiable] = valiables or list()
 
         for v in _valiables:
             if target.name == v.name:
@@ -65,5 +69,6 @@ class ReassignmentChecker:
                     col_offset=-1,
                     msg=f'RAC001 {v.type_} "{v.name}" (defined in line {v.lineno}) has been reassigned.',
                 )
+                break
 
-        yield from cls._check_names(targets, _valiables | {target})
+        yield from cls._check_names(targets, _valiables + [target])
